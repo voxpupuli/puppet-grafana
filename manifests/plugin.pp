585761bdca0a0c986dc975b8662cc544e58daef5
Geoff Crompton <Geoff Crompton geoffc@trinity.unimelb.edu.au>
2017-09-19 15:10:17 +1000

    Fix path to grafana-cli in manifests/plugin.pp.
    On my Debian 8 system the grafana package has grafana-cli under /usr/sbin,
    not /usr/bin.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -4,7 +4,7 @@ define grafana::plugin(
   String $plugin = $title,
 ){
   exec{"install ${plugin}":
-    command => "/usr/bin/grafana-cli plugins install ${plugin}",
+    command => "/usr/sbin/grafana-cli plugins install ${plugin}",
     creates => "/var/lib/grafana/plugins/${plugin}",
   }
 }
