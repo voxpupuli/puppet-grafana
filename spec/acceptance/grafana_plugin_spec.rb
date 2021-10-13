cf0582512ecc440fbb8cb92caa466f0cbe00e496
Stephan Eicher <stephan.eicher@check24.de>
2021-10-13 10:43:04 +0200

    Add package provider for improved plugin handling #178

--- spec/acceptance/grafana_plugin_spec.rb
+++ spec/acceptance/grafana_plugin_spec.rb
@@ -95,5 +95,35 @@ supported_versions.each do |grafana_version|
         end
       end
     end
+
+    context 'grafana plugins' do
+      it 'installs specific version' do
+        pp = <<-EOS
+        class { 'grafana':
+          version => "#{grafana_version}"
+        }
+
+        package { 'grafana-simple-json-datasource':
+          ensure   => '1.4.0',
+          provider => 'grafana',
+        }
+        EOS
+        # Run it twice and test for idempotency
+        apply_manifest(pp, catch_failures: true)
+        apply_manifest(pp, catch_changes: true)
+      end
+
+      it 'does have the plugin with specific version' do
+        shell('grafana-cli plugins ls') do |r|
+          expect(r.stdout).to match(%r{grafana-simple-json-datasource})
+          expect(r.stdout).to match(%r{1\.4\.0})
+        end
+      end
+
+      describe service('grafana-server') do
+        it { is_expected.to be_enabled }
+        it { is_expected.to be_running }
+      end
+    end
   end
 end
